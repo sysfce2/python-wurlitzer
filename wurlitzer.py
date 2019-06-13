44b6bfc6dcce2a17a72fad5366c62e764907c506
Min RK <benjaminrk@gmail.com>
2019-06-13 10:49:36 +0200

    Bump version: 1.0.3.dev → 1.0.3

--- wurlitzer.py
+++ wurlitzer.py
@@ -4,7 +4,7 @@ Use `wurlitzer.pipes` or `wurlitzer.sys_pipes` as context managers.
 """
 from __future__ import print_function
 
-__version__ = '1.0.3.dev'
+__version__ = '1.0.3'
 
 __all__ = [
     'pipes',
