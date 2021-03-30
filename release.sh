ef83f24fce155de2207ca7dbc9a98214b371d0e0
Min RK <benjaminrk@gmail.com>
2021-03-30 11:53:54 +0200

    update release script to run changelog

--- release.sh
+++ release.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-set -e
+set -eu
 
-bumpversion release --tag
-bumpversion patch
-git push --follow-tags
+before=$(git rev-parse HEAD)
+auto-changelog -v $VERSION
+pre-commit run --files CHANGELOG.md || true
+git add CHANGELOG.md
+git commit -m "changelog for $VERSION"
+bump2version release --tag --commit --new-version $VERSION
+bump2version patch
+git diff $before $VERSION
+echo "git push --follow-tags"
