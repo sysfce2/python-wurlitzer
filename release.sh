0854d308dadc6fc4841593bacfddeae1d4a1a905
Min RK <benjaminrk@gmail.com>
2024-06-12 12:26:36 +0200

    release with tbump

--- release.sh
+++ release.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-set -eu
+set -eux
 
+which tbump
 before=$(git rev-parse HEAD)
 auto-changelog -v $VERSION
 pre-commit run --files CHANGELOG.md || true
 git add CHANGELOG.md
 git commit -m "changelog for $VERSION"
-bump2version release --tag --commit --new-version $VERSION
-bump2version patch
-git diff $before $VERSION
-echo "git push --follow-tags"
+tbump $VERSION
